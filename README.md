27d4ba54903e681f8426f9c62712cd79041cbb73
Albino Moreira <albino.sistemas@gmail.com>
2021-04-18 17:20:20 -0300

    Configurações iniciais projeto Especialista Jpa

---
+++ README.md
@@ -0,0 +1,21 @@
+![EJPA](https://assets.algaworks.com/portal/thumbnails/especialista-jpa.png)
+
+# Treinamento Especialista JPA  
+  
+Código referente aos estudo do curso [Especialista PJA](https://cafe.algaworks.com/ejpa-lista-espera/) da algaworks.    
+  
+## Principais Características  
+  
+Projeto conta com as seguintes tecnologias:  
+  
+* Java 12
+* JPA 2
+* Hibernate 5.4.3
+* Mysql  
+* Maven
+* Junit
+     
+## Créditos   
+
+Copyright (C) 2020-2021 by Albino Moreira dos Santos
+* Albino Moreira dos Santos | [@albinoueg](https://github.com/albinoueg) | albino.santos@sefaz.go.gov.br    
---
+++ src/main/java/br/com/albinomoreira/ecommerce/util/IniciarUnidadeDePersistencia.java
@@ -0,0 +1,22 @@
+package br.com.albinomoreira.ecommerce.util;
+
+import br.com.albinomoreira.ecommerce.model.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class IniciarUnidadeDePersistencia {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(produto.getNome());
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
