7c3f34f8c219eb0da25896725081e570b99b51c4
Albino Moreira <albino.sistemas@gmail.com>
2021-04-20 18:59:09 -0300

    3.5. Mapeando enumerações com @Enumerated

--- src/main/java/br/com/albinomoreira/ecommerce/model/Pedido.java
+++ src/main/java/br/com/albinomoreira/ecommerce/model/Pedido.java
@@ -5,10 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -29,5 +26,6 @@ public class Pedido {
     @Column(name = "nota_fiscal_id")
     private Integer notaFiscalId;
     private BigDecimal total;
+    @Enumerated(EnumType.STRING)
     private StatusPedido status;
 }
---
+++ src/test/java/br/com/albinomoreira/ecommerce/mapeamentobasico/MapeandoEnumeracoesTest.java
@@ -0,0 +1,27 @@
+package br.com.albinomoreira.ecommerce.mapeamentobasico;
+
+import br.com.albinomoreira.ecommerce.EntityManagerTest;
+import br.com.albinomoreira.ecommerce.enums.SexoCliente;
+import br.com.albinomoreira.ecommerce.model.Cliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapeandoEnumeracoesTest extends EntityManagerTest {
+
+    @Test
+    public void testarEnum() {
+        Cliente cliente = new Cliente();
+        cliente.setId(4);
+        cliente.setNome("José Mineiro");
+        cliente.setSexo(SexoCliente.MASCULINO);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(clienteVerificacao);
+    }
+}
