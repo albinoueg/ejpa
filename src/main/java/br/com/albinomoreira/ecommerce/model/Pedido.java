e2e4ee5f4c8e1791fb7be35061a28d6594e28f0d
Albino Moreira <albino.sistemas@gmail.com>
2021-04-20 19:40:45 -0300

    3.10. Configurando geração de identificador com a estratégia IDENTITY

--- src/main/java/br/com/albinomoreira/ecommerce/model/Pedido.java
+++ src/main/java/br/com/albinomoreira/ecommerce/model/Pedido.java
@@ -18,6 +18,7 @@ public class Pedido {
 
     @EqualsAndHashCode.Include
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "data_pedido")
     private LocalDateTime dataPedido;
