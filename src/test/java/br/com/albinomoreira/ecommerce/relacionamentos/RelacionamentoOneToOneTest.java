42f154c2bc09e8b70bc3fa90a13b2be8b0903bb7
Albino Moreira <albino.sistemas@gmail.com>
2021-04-21 16:47:32 -0300

    4.10. Exercício: mapeando relacionamentos um-para-um

--- src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
+++ src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RelacionamentoOneToOneTest.java
@@ -2,10 +2,13 @@ package br.com.albinomoreira.ecommerce.relacionamentos;
 
 import br.com.albinomoreira.ecommerce.EntityManagerTest;
 import br.com.albinomoreira.ecommerce.enums.StatusPagamento;
+import br.com.albinomoreira.ecommerce.model.NotaFiscal;
 import br.com.albinomoreira.ecommerce.model.PagamentoCartao;
 import br.com.albinomoreira.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Date;
 
 public class RelacionamentoOneToOneTest extends EntityManagerTest {
 
@@ -26,5 +29,24 @@ public class RelacionamentoOneToOneTest extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         Assert.assertNotNull(pedidoVerificacao.getPagamentoCartao());
+    }
+
+    @Test
+    public void verificarRelacionamentoPedidoNotaFiscal() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setXml("TESTE");
+        notaFiscal.setDataEmissao(new Date());
+        notaFiscal.setPedido(pedido);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(notaFiscal);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getNotaFiscal());
     }
 }
