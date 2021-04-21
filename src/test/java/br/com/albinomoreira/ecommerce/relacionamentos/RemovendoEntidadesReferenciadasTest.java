e3fdd0703daec3096dbf7060271456d8cc7d8a23
Albino Moreira <albino.sistemas@gmail.com>
2021-04-21 16:24:02 -0300

    4.7. Removendo objetos referenciados por outras entidades

---
+++ src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RemovendoEntidadesReferenciadasTest.java
@@ -0,0 +1,26 @@
+package br.com.albinomoreira.ecommerce.relacionamentos;
+
+import br.com.albinomoreira.ecommerce.EntityManagerTest;
+import br.com.albinomoreira.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemovendoEntidadesReferenciadasTest extends EntityManagerTest {
+
+    @Test
+    public void removerEntidadeRelacionada() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItensPedidos().isEmpty());
+
+        entityManager.getTransaction().begin();
+        pedido.getItensPedidos().forEach(i -> entityManager.remove(i));
+        entityManager.remove(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, 1);
+        Assert.assertNull(pedidoVerificacao);
+    }
+}
