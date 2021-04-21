10bd500fcdc84be3284c278311515e742cd40a8f
Albino Moreira <albino.sistemas@gmail.com>
2021-04-21 15:54:45 -0300

    4.3. Exercício: mapeando relacionamentos muitos-para-um

--- src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -3,7 +3,9 @@ package br.com.albinomoreira.ecommerce.relacionamentos;
 import br.com.albinomoreira.ecommerce.EntityManagerTest;
 import br.com.albinomoreira.ecommerce.enums.StatusPedido;
 import br.com.albinomoreira.ecommerce.model.Cliente;
+import br.com.albinomoreira.ecommerce.model.ItemPedido;
 import br.com.albinomoreira.ecommerce.model.Pedido;
+import br.com.albinomoreira.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -25,6 +27,34 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
         entityManager.getTransaction().begin();
         entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getCliente());
+    }
+
+    @Test
+    public void verificarRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
         entityManager.getTransaction().commit();
 
         entityManager.clear();
