1b2a41bde9c846a815fd6e89f20265553a8ea6a4
Albino Moreira <albino.sistemas@gmail.com>
2021-04-30 21:34:24 -0300

    6.3. Mapeando chave composta com @IdClass

--- src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/br/com/albinomoreira/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -2,10 +2,7 @@ package br.com.albinomoreira.ecommerce.relacionamentos;
 
 import br.com.albinomoreira.ecommerce.EntityManagerTest;
 import br.com.albinomoreira.ecommerce.enums.StatusPedido;
-import br.com.albinomoreira.ecommerce.model.Cliente;
-import br.com.albinomoreira.ecommerce.model.ItemPedido;
-import br.com.albinomoreira.ecommerce.model.Pedido;
-import br.com.albinomoreira.ecommerce.model.Produto;
+import br.com.albinomoreira.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -37,6 +34,7 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
     @Test
     public void verificarRelacionamentoItemPedido() {
+        entityManager.getTransaction().begin();
         Cliente cliente = entityManager.find(Cliente.class, 1);
         Produto produto = entityManager.find(Produto.class, 1);
 
@@ -46,20 +44,24 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
         pedido.setTotal(BigDecimal.TEN);
         pedido.setCliente(cliente);
 
+        entityManager.persist(pedido);
+
+        entityManager.flush();
+
         ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPedidoId(pedido.getId());
+        itemPedido.setProdutoId(produto.getId());
         itemPedido.setPrecoProduto(produto.getPreco());
         itemPedido.setQuantidade(1);
         itemPedido.setPedido(pedido);
         itemPedido.setProduto(produto);
 
-        entityManager.getTransaction().begin();
-        entityManager.persist(pedido);
         entityManager.persist(itemPedido);
         entityManager.getTransaction().commit();
 
         entityManager.clear();
 
-        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, new ItemPedidoId(1, 1));
         Assert.assertNotNull(itemPedidoVerificacao.getPedido());
         Assert.assertNotNull(itemPedidoVerificacao.getProduto());
     }
